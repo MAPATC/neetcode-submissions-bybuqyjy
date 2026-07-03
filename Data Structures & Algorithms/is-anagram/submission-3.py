5a5fd27ca0b39fda1c6d3e5f878f79b099cad893
JRWIX <surovyatkinvlad@gmail.com>
2026-07-03 12:52:26 +0300

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sort_s = "".join(sorted(s))
+        sort_t = "".join(sorted(t))
+
+        if len(sort_s) != len(sort_t):
+            return False
+
+        for i in range(len(sort_s)):
+            if sort_s[i] != sort_t[i]:
+                return False
+        return True
